878049bc7002ca63ed5738a800c3ec3ba1c3c818
Viraj Lakshitha Bandara <62491019+viraj-lakshitha@users.noreply.github.com>
2022-07-15 21:55:01 +0530

    Enable for the offline mode (Just for testing)

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,25 @@
-console.log('Service worker running on background')
+// Store data in cache
+let cacheData = "appV1";
+this.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open(cacheData).then((cache) => {
+            cache.addAll([
+                '/static/main.chunk.js',
+                '/static/js/0.chunk.js',
+                '/static/js/bundle.js',
+                '/index.js',
+                '/',
+            ])
+        })
+    )
+})
+
+this.addEventListener("fetch", (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            if (response) {
+                return response
+            }
+        })
+    )
+})
