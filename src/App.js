9818bf2e0c040d4b60228fd706a59dec5e8ed4e6
Viraj Lakshitha Bandara <62491019+viraj-lakshitha@users.noreply.github.com>
2022-07-15 18:51:03 +0530

    Create simple UI and install react-router-dom package for navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import { Route, BrowserRouter, Routes, Link } from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import "./App.css";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+      <div className="d-flex align-items-center justify-content-between px-2 w-100 custom-navbar">
+        <div className="nav-brand">Progressive Web App</div>
+        <div className="d-flex nav-item">
+          <Link className="navbar-item me-1" to="/">
+            Home
+          </Link>
+          <Link className="navbar-item mx-1" to="/about">
+            About
+          </Link>
+          <Link className="navbar-item ms-1" to="/profile">
+            Profile
+          </Link>
+        </div>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
